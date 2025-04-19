499d5636625ced0d0c3868113b091f4297796347
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-04-19 19:34:48 +0800

    Added category component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizCategoryComponent } from '../quiz-category/quiz-category.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, QuizCategoryComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by category" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-quiz-category></app-quiz-category>
     </section>
   `,
   styleUrls: ['./home.component.css'],
---
+++ src/app/quiz-category/quiz-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-category',
+  imports: [],
+  templateUrl: './quiz-category.component.html',
+  styleUrl: './quiz-category.component.css'
+})
+export class QuizCategoryComponent {
+
+}

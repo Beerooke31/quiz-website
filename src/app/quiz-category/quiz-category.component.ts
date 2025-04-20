85453868425673392fed694b06379a1e672dfa6b
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-04-20 17:48:01 +0800

    Added images for quiz categories

--- src/app/quiz-category/quiz-category.component.ts
+++ src/app/quiz-category/quiz-category.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizCategory } from '../quizcategory';
 @Component({
   selector: 'app-quiz-category',
-  imports: [],
-  templateUrl: './quiz-category.component.html',
-  styleUrl: './quiz-category.component.css'
+  imports: [CommonModule],
+  template: ` <section class="quiz">
+    <img
+      class="quiz-picture"
+      [src]="quizCategory.image"
+      alt="Image of type of {{ quizCategory.type }}"
+      crossorigin
+    />
+    <h2 class="quiz-heading">{{ quizCategory.title }}</h2>
+    <p class="quiz-type">{{ quizCategory.type }}</p>
+  </section>`,
+  styleUrls: ['./quiz-category.component.css'],
 })
 export class QuizCategoryComponent {
-
+  @Input() quizCategory!: QuizCategory;
 }
---
+++ src/app/quizcategory.ts
@@ -0,0 +1,7 @@
+export interface QuizCategory {
+  id: number;
+  title: string;
+  image: string;
+  date: string;
+  type: string;
+}
